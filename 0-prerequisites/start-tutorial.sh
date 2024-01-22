1252436b8378dba5234cb351970fba77bfb630eb
José Maia <jose.maia@ctw.bmwgroup.com>
2024-01-22 14:05:43 +0000

    Prepare stage 01

--- 0-prerequisites/start-tutorial.sh
+++ 0-prerequisites/start-tutorial.sh
@@ -11,6 +11,9 @@ then
     read name
     az group create --name "$name-terraform-tutorial" --location westeurope
     az storage account create --name "$name-terraform-state" --resource-group "$name-terraform-tutorial" --location westeurope --sku Standard_LRS
+    az storage container create --name terraform-state --account-name "$name-terraform-state"
+    ACCOUNT_KEY=$(az storage account keys list --resource-group "$name-terraform-state" --account-name "$name-terraform-state" --query '[0].value' -o tsv)
+    export ARM_ACCESS_KEY=$ACCOUNT_KEY
     echo "Resource group and storage account created. You can move on to step 1."
     exit 0
 else
---
+++ 1-getting-started/data.tf
@@ -0,0 +1,4 @@
+data "azurerm_resource_group" "tutorial_rg" {
+  name     = "$name-terraform-tutorial"
+  location = "westeurope"
+}
