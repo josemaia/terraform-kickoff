532be81013ed2af72f7a957c985760b0ef3f4ac6
José Maia <jose.maia@ctw.bmwgroup.com>
2024-01-22 14:18:46 +0000

    Review hyphens

--- 0-prerequisites/start-tutorial.sh
+++ 0-prerequisites/start-tutorial.sh
@@ -10,10 +10,10 @@ then
     echo "Please input your first and last name (e.g. josemaia), which will be used to name the resources in Azure:"
     read name
     az group create --name "$name-terraform-tutorial" --location westeurope
-    az storage account create --name "$name-terraform-state" --resource-group "$name-terraform-tutorial" --location westeurope --sku Standard_LRS
-    az storage container create --name terraform-state --account-name "$name-terraform-state"
-    ACCOUNT_KEY=$(az storage account keys list --resource-group "$name-terraform-state" --account-name "$name-terraform-state" --query '[0].value' -o tsv)
-    export ARM_ACCESS_KEY=$ACCOUNT_KEY
+    az storage account create --name "$name"terraformstate --resource-group "$name-terraform-tutorial" --location westeurope --sku Standard_LRS
+    az storage container create --name terraform-state --account-name "$name"terraformstate
+    ACCOUNT_KEY=$(az storage account keys list --resource-group "$name-terraform-tutorial" --account-name "$name"terraformstate --query '[0].value' -o tsv)
+    echo "Your storage account key is $ACCOUNT_KEY. Set it to env var ARM_ACCESS_KEY before running the next step."
     echo "Resource group and storage account created. You can move on to step 1."
     exit 0
 else
